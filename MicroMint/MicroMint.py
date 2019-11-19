fa503a1b71d7d463d2286c598b0e411b945bdf51
PetterBerntsson <iprovedtheriemannhypothesis@gmail.com>
2019-11-19 17:02:47 +0100

    fungerande första simulering

--- MicroMint/MicroMint.py
+++ MicroMint/MicroMint.py
@@ -1,6 +1,41 @@
-def main():
-    print("MicroMint")
+from random import random
 
+coin_dict = {}
+
+# bits per coin
+u = 16
+
+# pre-images needed per coin (bucket depth)
+k = 2
+
+# coins to generate
+c = 10000
+
+
+def main():
+    # buckets to generate
+    b = 2**u
+
+
+    find_collisions()
+
+
+
+def find_collisions():
+    b = 2 ** u
+
+    collisions = 0
+    iterations = 0
+    while collisions < c:
+        i = int(random() * b)
+        coin_dict[i] = coin_dict.get(i, 0) + 1
+        iterations = iterations + 1
+
+        if coin_dict[i] >= k:
+            collisions = collisions + 1
+
+    print("coins found: " + str(collisions))
+    print("iterations: " + str(iterations))
 
 if __name__ == "__main__":
     main()

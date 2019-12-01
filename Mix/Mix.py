e6f4469105745edf8c2feb77878d79c736706334
PetterBerntsson <iprovedtheriemannhypothesis@gmail.com>
2019-12-01 17:49:15 +0100

    reformated code

--- Mix/Mix.py
+++ Mix/Mix.py
@@ -1,31 +1,20 @@
-#!/usr/bin/env python
-
 from pcapfile import savefile
+import sys
 import re
 
 test = 2
+filepath = ""
 
 if test == 1:
-    testcap = open('cia.log.1337.pcap', 'rb')
-    capfile = savefile.load_savefile(testcap, layers=2, verbose=True)
-    testcap.close()
-
+    filepath = 'cia.log.1337.pcap'
     nazirIP = "159.237.13.37"
     mixIP = "94.147.150.188"
-
     nbrPartners = 2
-
-
 else:
-
-    testcap = open('cia.log.1339.pcap', 'rb')
-    capfile = savefile.load_savefile(testcap, layers=2, verbose=True)
-    testcap.close()
-
+    filepath = 'cia.log.1339.pcap'
     nazirIP = "161.53.13.37"
     mixIP = "11.192.206.171"
     nbrPartners = 12
-
 
 long_set = list()
 old_ip_dst = ""
@@ -34,43 +23,45 @@ batch_set = set()
 source_set = set()
 new_batch = False
 
-for pkt in capfile.packets:
+# --------------------------------------- LOAD BATCHES ----------------------------------------------------------------
+with open(filepath, 'rb') as testcap:
+    capfile = savefile.load_savefile(testcap, layers=2, verbose=True)
+    testcap.close()
+    for pkt in capfile.packets:
 
-    timestamp = pkt.timestamp
-    # all data is ASCII encoded (byte arrays). If we want to compare with strings
-    # we need to decode the byte arrays into UTF8 coded strings
-    eth_src = pkt.packet.src.decode('UTF8')
-    eth_dst = pkt.packet.dst.decode('UTF8')
-    ip_src = pkt.packet.payload.src.decode('UTF8')
-    ip_dst = pkt.packet.payload.dst.decode('UTF8')
+        timestamp = pkt.timestamp
+        # all data is ASCII encoded (byte arrays). If we want to compare with strings
+        # we need to decode the byte arrays into UTF8 coded strings
+        eth_src = pkt.packet.src.decode('UTF8')
+        eth_dst = pkt.packet.dst.decode('UTF8')
+        ip_src = pkt.packet.payload.src.decode('UTF8')
+        ip_dst = pkt.packet.payload.dst.decode('UTF8')
 
+        if ip_dst == mixIP:
+            if new_batch:
+                # We will see if the batch is worth adding to the collection
+                if nazirIP in source_set:
+                    long_set.append(batch_set.copy())
+                batch_set.clear()
+                source_set.clear()
+                new_batch = False
+            source_set.add(ip_src)
+        elif ip_src == mixIP:
 
-    if ip_dst == mixIP:
-        if new_batch:
-            # We will see if the batch is worth adding to the collection
-            if nazirIP in source_set:
-                long_set.append(batch_set.copy())
-            batch_set.clear()
-            source_set.clear()
-            new_batch = False
-        source_set.add(ip_src)
-    elif ip_src == mixIP:
+            # Some sort of trigger for the new batch, though the new batch started earlier
+            new_batch = True
+            batch_set.add(ip_dst)
+    # Last batch is skipped otherwise
+    if nazirIP in source_set:
+        long_set.append(batch_set)
 
-        # Some sort of trigger for the new batch, though the new batch started earlier
-        new_batch = True
-        batch_set.add(ip_dst)
-# Last batch is skipped otherwise
-if nazirIP in source_set:
-    long_set.append(batch_set)
-
-    #print('{}\t\t{}\t{}\t{}\t{}'.format(timestamp, eth_src, eth_dst, ip_src, ip_dst))
-
+# --------------------------------------- SECOND PART OF LEARNING PHASE------------------------------------------------
 long_set_copy = long_set.copy()
 check_set = long_set_copy.pop(0)
 union_set = check_set.copy()
 mes_list = list()
 mes_list.append(check_set)
-
+# --------------------------------------- GET Number DISJOINT SETS = Number Partners ----------------------------------
 for batch in long_set_copy:
     if union_set.isdisjoint(batch):
         union_set = union_set.union(batch)
@@ -80,32 +71,22 @@ for batch in long_set_copy:
 
     if nbrPartners == 0:
         break
-print("Number ME sets: " + str(len(mes_list)))
-
-singleton_list = list()
+# --------------------------------------- RUN INTERSECTIONS -----------------------------------------------------------
+partners = list()
 
 for mes_set in mes_list:
 
-    # For some reason, you cant mutate a set you iterate
+    # For some reason, you cant mutate a set that is part of a list that you iterate
     temp_mes_set = mes_set.copy()
-
     rest_union = union_set.symmetric_difference(temp_mes_set.copy())
 
     for batch in long_set:
         if batch.isdisjoint(rest_union):
             temp_mes_set = temp_mes_set.intersection(batch)
 
+    partners.append(next(iter(temp_mes_set)))
 
-    singleton_list.append(temp_mes_set)
-
-
-
-partners = list()
-
-for unit_set in singleton_list:
-    partners.append(next(iter(unit_set)))
-
-print("Number partners: " + str(len(partners)))
+# --------------------------------------- SUM HEX VALUES AND PRINT DECIMAL VALUE --------------------------------------
 ip_lists = list()
 ip_hex = ""
 dec_sum = 0
@@ -116,15 +97,9 @@ for partner in partners:
     for ip_nbr in ip_list:
         ip_hex = ip_hex + hex(int(ip_nbr))[2:]
 
-
     dec_sum = dec_sum + int("0x" + ip_hex, 16)
     ip_hex = ""
 
+print("--------------- Answer --------------")
 print(dec_sum)
-
-
-
-
-
-print("-----------------------------------------------------------------------------------------------------")
-print('timestamp\t\teth src\t\t\teth dst\t\t\tIP src\t\tIP dst')
+print("-------------------------------------")

cbc8d322bb1e052e0f72e6abfdd9571ae32bc46e
PetterBerntsson <iprovedtheriemannhypothesis@gmail.com>
2019-12-07 20:02:24 +0100

    Pretty printing and file writing

--- CommitmentScheme/Commitment.py
+++ CommitmentScheme/Commitment.py
@@ -1,4 +1,5 @@
 import hashlib
+import os
 from collections import defaultdict
 import binascii
 
@@ -55,11 +56,13 @@ for x_length in range(1, x_limit):
 
     # No point to continue
     if collisions == 0:
-        print("breaking at run " + str(x_length))
+        print("-"*20 + "\nBreaking at run " + str(x_length) + ": Reached 0 collisions\n" + "-"*20)
         break
 
     collisions = 0
-    print("Run " + str(x_length))
+    print("Running... |" + "-"*x_length + "%" + " "*(x_limit - x_length) + "|")
+
+copy_coll_list = coll_list.copy()
 
 for i in range(1, len(coll_list) + 1):
     colls = coll_list.pop(0)
@@ -68,5 +71,15 @@ for i in range(1, len(coll_list) + 1):
     # "\t\t Out of " + str(16**i) + " "*(70 - len(str(16**i))) + "values \t" +
 
 print("\nRemaining values give probability 0%")
+print("Writing to file output.txt....")
+
+
+f = open("output.txt", 'w+')
+f.write("Probabilities (%): \n")
+for i in range(1, len(copy_coll_list) + 1):
+    colls = copy_coll_list.pop(0)
+    f.write(str(100*colls/min(2**i, 2**k_limit)) + "\n")
+f.close()
+print("Finished writing the file")
 
 # To break the binding property, we need to find a value k, where h(0,k1) = h(1,k2)

ad35daa1b46fd80e88a6613ffd54c8035a154fb7
PetterBerntsson <iprovedtheriemannhypothesis@gmail.com>
2019-12-07 20:07:37 +0100

    Refactored code

--- CommitmentScheme/Commitment.py
+++ CommitmentScheme/Commitment.py
@@ -1,23 +1,24 @@
 import hashlib
-import os
 from collections import defaultdict
-import binascii
-
-# v is either 1 or 0
-v = 0
 
 # bit limit
 k_limit = 16
 
-# after 24 it stops converging
-x_limit = 48
+# after 35 it stops converging
+x_limit = 36
 
-concat = ""
-coll_dict = defaultdict(int)
+# v is either 1 or 0
+v = 0
+# Our dictionaries for v= 1 || 0
 zero_dict = defaultdict(bool)
 one_dict = defaultdict(bool)
+
+# Collision list and collision counter
 coll_list = list()
 collisions = 0
+
+# Some useful strings
+concat = ""
 hash_res = ""
 
 for x_length in range(1, x_limit):
@@ -30,7 +31,7 @@ for x_length in range(1, x_limit):
     for k in range(2**k_limit):
 
         # We add space, though it makes no difference, but if v could be larger, "v0k0" could be equal to "v1k1",
-        # though "v0 k0" is never equal to "v1 k1". Basically we avoid "simple" collisions
+        # though "v0 k0" is never equal to "v1 k1". Basically we avoid "false" collisions
         concat = str(v) + " " + str(k)
 
         hash_res = hashlib.sha1(concat.encode()).hexdigest()
@@ -53,7 +54,6 @@ for x_length in range(1, x_limit):
 
     coll_list.append(collisions)
 
-
     # No point to continue
     if collisions == 0:
         print("-"*20 + "\nBreaking at run " + str(x_length) + ": Reached 0 collisions\n" + "-"*20)
@@ -64,16 +64,19 @@ for x_length in range(1, x_limit):
 
 copy_coll_list = coll_list.copy()
 
+
+# Printing result
 for i in range(1, len(coll_list) + 1):
     colls = coll_list.pop(0)
     print("X length = " + str(i) + " "*(3 - len(str(i))) + " - " + "Number collisions = " + str(colls) + " "*(10 - len(str(colls)))
-          +  "Probability: " + str(100*colls/min(2**i, 2**k_limit)) + "%")
+          + "Probability: " + str(100*colls/min(2**i, 2**k_limit)) + "%")
     # "\t\t Out of " + str(16**i) + " "*(70 - len(str(16**i))) + "values \t" +
 
 print("\nRemaining values give probability 0%")
 print("Writing to file output.txt....")
 
 
+# File writing
 f = open("output.txt", 'w+')
 f.write("Probabilities (%): \n")
 for i in range(1, len(copy_coll_list) + 1):

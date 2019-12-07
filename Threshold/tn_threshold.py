d792db18c22614a290ff1643395450b37bc85705
PetterBerntsson <iprovedtheriemannhypothesis@gmail.com>
2019-12-07 17:05:30 +0100

    Finalized everything

--- Threshold/tn_threshold.py
+++ Threshold/tn_threshold.py
@@ -1,28 +1,30 @@
-k_threshold = 3
-n_threshold = 8
+# Values are not needed
+# k_threshold = 3
+# n_threshold = 8
 
 
 # Private polynomial
 def p(x):
-    return 20 + 20*x + 11*(x**2) + 6*(x**3)
+    return 3 + 5*x + 10*(x**2) + 11*(x**3)
 
 
 # Private data points, probably not needed
-# data = [1, 161, 568, 1565, 3578, 7153, 12956, 21773]
+# data = [p(1), 161, 568, 1565, 3578, 7153, 12956, 21773]
 
 # Received data points f(1), index = participant index
-received_data = [p(1), 63, 49, 49, 54, 43]
+received_data = [p(1), 45, 39, 41, 55, 45, 41]
 
 # Received data points f(n), index = participant index
-interpolation_points = [sum(received_data), 2199, 4389, 12585]
+interpolation_points = [sum(received_data), 3313, 12939, 21430]
 
 # Each x represent the index of each participant that's part of K, order corresponding to their submitted data
 # Make sure to include 1, since we are always participating as 1
-x_vals = [1, 3, 4, 6]
+x_vals = [1, 3, 5, 6]
 # For the sake of clarity
 y_vals = interpolation_points
 
 
+# Some Lagrange interpolation
 def lagrange_interpolation(x, i, x_vals):
     y = 1.0
     for j in range(len(x_vals)):
@@ -37,7 +39,7 @@ def interpolation(x, x_vals, y_vals):
     return y
 
 
-# Truncates decimals
+# Just some printing
 print("-"*23)
 deactivation_code = interpolation(0, x_vals, y_vals)
 print("Deactivation Code: " + str(round(deactivation_code)))

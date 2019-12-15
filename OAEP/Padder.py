bd63736a148ca38660352b5b7174921342e4ee1e
PetterBerntsson <iprovedtheriemannhypothesis@gmail.com>
2019-12-15 18:28:10 +0100

    added working i2osp

---
+++ OAEP/Padder.py
@@ -0,0 +1,57 @@
+import math
+import hashlib
+
+
+mgfSeed = '0123456789abcdef'
+maskLen = 30
+
+# hLen = 20 for SHA-1
+hLen = 20
+
+
+# implemented according to https://tools.ietf.org/html/rfc8017#appendix-B.2.1
+def mgf1(mgfSeed, maskLen):
+
+    t = ''
+
+    for counter in range(math.ceil(maskLen/hLen)):
+        print("iteration: " + str(counter))
+
+        # xLen is always 4
+        c = i2osp(counter, 4)
+
+        concat = mgfSeed + c
+        print(t)
+        t = t + hashlib.sha1(concat.encode()).hexdigest()
+
+    return t[:maskLen*2]
+
+
+# implemented according to https://tools.ietf.org/html/rfc8017#section-4.1
+# modified to return hex string representation
+def i2osp(x, xLen):
+    if x >= 256**xLen:
+        raise ValueError("integer too large")
+    digits = []
+
+    while x:
+        digits.append(int(x % 256))
+        x //= 256
+
+    for i in range(xLen - len(digits)):
+        digits.append(0)
+
+    digits = digits[::-1]
+
+    # return digits if you want a list representation
+    tot_hex_string = ''
+
+    for i in range(len(digits)):
+        hex_string = hex(digits[i])[2:]
+        if len(hex_string) < 2:
+            hex_string = '0' + hex_string
+        tot_hex_string = tot_hex_string + hex_string
+
+    return tot_hex_string
+
+print(mgf1(mgfSeed, maskLen))

